f0b6c43ed5f74ea9d1de36791557cf2e0a397861
Jindong Yue <jindong.yue@nxp.com>
2024-12-27 12:46:12 +0800

    MA-23284 audio: Update the default period size and count
    The audio framework writes mBufferSizeFrames each time.
    Set the default period size as mBufferSizeFrames to
    reduce underrun.
    
    The mBufferSizeFrames is calculated by getNominalLatencyMs
    and the sample rate, typical values:
     - kLatencyMs: 5
     - sample rate: 48000
     - mBufferSizeFrames: 240
     - period size: 240
     - period count: 4
    
    Change-Id: I1010384aa460519919dadbec84e6bd00cf6c5290
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27121
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/primary/StreamPrimary.cpp
+++ audio/default/primary/StreamPrimary.cpp
@@ -36,6 +36,7 @@ extern "C" {
 #include "alsa_device_profile.h"
 }
 
+#define DEFAULT_PERIOD_COUNT 4
 #define LPA_PERIOD_MS 500
 #define LPA_BUFFER_SECOND 20
 
@@ -361,6 +362,9 @@ std::vector<alsa::DeviceProfile> StreamPrimary::getDeviceProfiles() {
             mIsS16ToS24 = false;
             mConfig = mSavedConfig;
         }
+
+        mConfig->period_size = mBufferSizeFrames;
+        mConfig->period_count = DEFAULT_PERIOD_COUNT;
 
         if (card->out_period_size) {
             mConfig->period_size = card->out_period_size;
