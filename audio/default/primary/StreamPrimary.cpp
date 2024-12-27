0811e4a2d36f799bf4885f36129736f6160ce6f3
Jindong Yue <jindong.yue@nxp.com>
2024-12-27 10:30:03 +0800

    MA-23223 audio: Update lpa period size for non-primary stream
    There are several seconds delay before the sound can be heard
    when playing media files with Cactus/Gallery, not DirectAudioPlayer.
    
    Before the alsa buffer reaches the start_threshold(10s in LPA case),
    the pcm_write returns immediately, the mixer thread will sleep
    to avoid processing too fast (see mThreadThrottle).
    
    Fix it by using the default period size and count for primary stream.
    
    Change-Id: I15fc6ace1a46f921635610ce6594998be1accfbc
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27120
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/primary/StreamPrimary.cpp
+++ audio/default/primary/StreamPrimary.cpp
@@ -284,7 +284,7 @@ void StreamPrimary::stop() {
 }
 
 ::android::status_t StreamPrimary::refinePosition(StreamDescriptor::Position* position) {
-    if (property_get_int32("vendor.audio.lpa.enable", 0) ||
+    if ((property_get_int32("vendor.audio.lpa.enable", 0) && !mPrimary) ||
             (getContext().getFormat().encoding == "audio/vnd.sony.dsd")) {
         return StreamAlsa::refinePosition(position);
     }
@@ -369,11 +369,11 @@ std::vector<alsa::DeviceProfile> StreamPrimary::getDeviceProfiles() {
             mConfig->period_count = card->out_period_count;
         }
 
-        if (property_get_int32("vendor.audio.lpa.enable", 0)) {
+        if (property_get_int32("vendor.audio.lpa.enable", 0) && !mPrimary) {
             mConfig->period_size = mConfig->rate * LPA_PERIOD_MS / 1000;
             mConfig->period_count = LPA_BUFFER_SECOND * 1000 / LPA_PERIOD_MS;
-            if (!mPrimary)
-                mHardwarePause = true;
+            mHardwarePause = true;
+            LOG(INFO) << __func__ << ": Force set period size as " << LPA_PERIOD_MS << "ms for LPA";
         }
     }
 
