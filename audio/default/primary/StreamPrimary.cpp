bb8f3b9b2781834a89eea081c07154b31be33388
Jindong Yue <jindong.yue@nxp.com>
2025-02-28 15:01:52 +0800

    MA-23500 audio: Fix format conversion error from S32 to S16
    When the channel is 2, the data count in memcpy is wrong.
    It shoud be multiplied by the channel number, otherwise
    the sound is abnormal.
    
    Change-Id: I5080d65f7edff5cd1c927f63e26c11668be8c257
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27648
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/primary/StreamPrimary.cpp
+++ audio/default/primary/StreamPrimary.cpp
@@ -300,7 +300,7 @@ void StreamPrimary::stop() {
 
         RETURN_STATUS_IF_ERROR(
                 StreamAlsa::transfer(src.get(), frameCount * 2, actualFrameCount, latencyMs));
-        memcpy_to_i16_from_i32(dst, src.get(), frameCount);
+        memcpy_to_i16_from_i32(dst, src.get(), frameCount * channels);
 
         *actualFrameCount /= 2;
 
