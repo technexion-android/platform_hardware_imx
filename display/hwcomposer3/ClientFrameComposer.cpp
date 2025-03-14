445ce406fa8759cbf77215eef670fe9f9e7bdf64
Bo Zhang <bo.zhang@nxp.com>
2025-03-14 19:28:18 +0900

    MA-23544-2 Add g2d time consumption debug log
    Add macro DEBUG_DUMP_G2D_CONSUMPTION to control if print the g2d time
    consumption or not.
    
    Change-Id: Ic46d5b628a3e8099c80bb86f24c8ecd79281c51e
    Signed-off-by: Bo Zhang <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27773
    Reviewed-by: Jessie Hao <juan.hao@nxp.com>
    (cherry picked from commit 8cdde9d5233e1ed2e7be3b6956213d8b34a181d5)
    Reviewed-on: http://androidsource.nxp.com/project/27795

--- display/hwcomposer3/ClientFrameComposer.cpp
+++ display/hwcomposer3/ClientFrameComposer.cpp
@@ -555,7 +555,20 @@ HWC3::Error ClientFrameComposer::presentDisplay(
             ALOGE("%s: display:%d failed to get composer target", __FUNCTION__, displayId);
             return error;
         }
+#ifdef DEBUG_DUMP_G2D_CONSUMPTION
+        nsecs_t composeStart = systemTime(CLOCK_MONOTONIC);
+#endif
         auto [ret, composeFence] = mG2dComposer->composeLayers(layersForComposition, renderTarget);
+#ifdef DEBUG_DUMP_G2D_CONSUMPTION
+        nsecs_t composeEnd = systemTime(CLOCK_MONOTONIC);
+        totalCostTime += composeEnd - composeStart;
+        composeCount++;
+        if (composeCount >= 60) {
+            ALOGI("%s: average g2d cost time:%3.3f ms", __FUNCTION__, totalCostTime / (composeCount * 1000000.0));
+            totalCostTime = 0;
+            composeCount = 0;
+        }
+#endif
         if (ret) {
             fbInFence = std::move(composeFence);
             if (CC_UNLIKELY(atrace_is_tag_enabled(ATRACE_TAG_GRAPHICS) && fbInFence.ok())) {
