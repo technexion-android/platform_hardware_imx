0d37b213c674dbe9fb7f6a53b0aa506062fd5158
Jindong Yue <jindong.yue@nxp.com>
2025-03-24 16:00:11 +0800

    MA-23581-1 audio: Support hfp
    Add the support of hfp feature.
    Two threads are created to move data between
    the board and bt-sco card.
    
    Uplink:
    mic -> bt-sco
    
    Downlink:
    bt-sco -> speaker
    
    Currently it only supports 16000,mono,16 bit pcm.
    It also assumes that the speaker and microphone are free.
    
    Change-Id: I99de28f5270bf0fe43182672b392f6dafaf9cbfa
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27842
    Reviewed-by: Jiri Rezler <jiri.rezler@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27956
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/include/core-impl/Bluetooth.h
+++ audio/default/include/core-impl/Bluetooth.h
@@ -19,6 +19,8 @@
 #include <aidl/android/hardware/audio/core/BnBluetooth.h>
 #include <aidl/android/hardware/audio/core/BnBluetoothA2dp.h>
 #include <aidl/android/hardware/audio/core/BnBluetoothLe.h>
+#include <core-impl/AudioCardManager.h>
+#include <tinyalsa/asoundlib.h>
 
 namespace aidl::android::hardware::audio::core {
 
@@ -41,6 +43,27 @@ class Bluetooth : public BnBluetooth {
 
     ScoConfig mScoConfig;
     HfpConfig mHfpConfig;
+
+  private:
+    struct pcm_config pcm_config_hfp;
+    struct pcm *pcm_speaker_out = NULL;
+    struct pcm *pcm_mic_in = NULL;
+    struct pcm *pcm_sco_out = NULL;
+    struct pcm *pcm_sco_in = NULL;
+
+    pthread_t tid_uplink = 0;
+    pthread_t tid_downlink = 0;
+    bool uplink_running = false;
+    bool downlink_running = false;
+
+    void startHfp();
+    void stopHfp();
+    static void *uplink_task(void *arg);
+    void *uplink_task_impl();
+    static void *downlink_task(void *arg);
+    void *downlink_task_impl();
+    int openPcmForDevice(const audio_devices_t& audioDevice, unsigned int flags,
+        struct pcm_config *config, struct pcm** pcm);
 };
 
 class BluetoothA2dp : public BnBluetoothA2dp, public ParamChangeHandler {
