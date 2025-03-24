8d5ed5d6f5490a1acaf642c9cf86c20076e3e8ee
Jindong Yue <jindong.yue@nxp.com>
2025-03-24 16:00:11 +0800

    MA-23581-1 audio: Support hfp
    Add the support of hfp feature.
    Two threads are created to move data between
    the board and bt-sco card.
    
    Uplink:
    mic -> bt-sco
    
    Downlink:
    bt-sco -> speaker
    
    Currently it only supports 16000,mono,16 bit pcm.
    It also assumes that the speaker and microphone are free.
    
    Change-Id: I99de28f5270bf0fe43182672b392f6dafaf9cbfa
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27842
    Reviewed-by: Jiri Rezler <jiri.rezler@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27956
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/Bluetooth.cpp
+++ audio/default/Bluetooth.cpp
@@ -33,6 +33,16 @@ Bluetooth::Bluetooth() {
     mHfpConfig.isEnabled = Boolean{false};
     mHfpConfig.sampleRate = Int{8000};
     mHfpConfig.volume = Float{HfpConfig::VOLUME_MAX};
+
+    pcm_config_hfp = {
+        .channels = 1,
+        .rate = 16000,
+        .period_size = 256,
+        .period_count = 4,
+        .format = PCM_FORMAT_S16_LE,
+        .start_threshold = 0,
+        .avail_min = 0,
+    };
 }
 
 ndk::ScopedAStatus Bluetooth::setScoConfig(const ScoConfig& in_config, ScoConfig* _aidl_return) {
@@ -54,6 +64,198 @@ ndk::ScopedAStatus Bluetooth::setScoConfig(const ScoConfig& in_config, ScoConfig
     return ndk::ScopedAStatus::ok();
 }
 
+int Bluetooth::openPcmForDevice(const audio_devices_t& audioDevice, unsigned int flags,
+        struct pcm_config *config, struct pcm** pcm)
+{
+    if (!config || !pcm) {
+        return -EINVAL;
+    }
+    auto card = AudioCardManager::getCardForDevice(audioDevice);
+    if (!card) {
+        LOG(ERROR) << "card is not found";
+        return -EINVAL;
+    }
+
+    *pcm = pcm_open(card->card, 0, flags, config);
+    if (!*pcm) {
+        LOG(ERROR) << "pcm_open() failed: " << pcm_get_error(*pcm);
+        return -EBUSY;
+    }
+    if (!pcm_is_ready(*pcm)) {
+        LOG(ERROR) << "pcm_is_ready() failed: " << pcm_get_error(*pcm);
+        pcm_close(*pcm);
+        *pcm = NULL;
+        return -EBUSY;
+    }
+
+    LOG(INFO) << "  channels: " << config->channels;
+    LOG(INFO) << "  rate: " << config->rate;
+    LOG(INFO) << "  period_size: " << config->period_size;
+    LOG(INFO) << "  period_count: " << config->period_count;
+    LOG(INFO) << "  format: " << config->format;
+    return 0;
+}
+
+void Bluetooth::stopHfp() {
+    LOG(DEBUG) << __func__;
+    if (tid_uplink) {
+        uplink_running = false;
+        pthread_join(tid_uplink, NULL);
+        tid_uplink = 0;
+    }
+
+    if (tid_downlink) {
+        downlink_running = false;
+        pthread_join(tid_downlink, NULL);
+        tid_downlink = 0;
+    }
+
+    if (pcm_speaker_out) {
+        pcm_close(pcm_speaker_out);
+        pcm_speaker_out = NULL;
+    }
+
+    if (pcm_mic_in) {
+        pcm_close(pcm_mic_in);
+        pcm_mic_in = NULL;
+    }
+
+    if (pcm_sco_out) {
+        pcm_close(pcm_sco_out);
+        pcm_sco_out = NULL;
+    }
+
+    if (pcm_sco_in) {
+        pcm_close(pcm_sco_in);
+        pcm_sco_in = NULL;
+    }
+}
+
+void* Bluetooth::uplink_task(void* arg) {
+    Bluetooth* instance = static_cast<Bluetooth*>(arg);
+    instance->uplink_task_impl();
+    return nullptr;
+}
+
+void *Bluetooth::uplink_task_impl()
+{
+    int ret = 0;
+    size_t size = pcm_config_hfp.period_size;
+    void *buffer = malloc(size);
+
+    if (!buffer) {
+        LOG(ERROR) << __func__ << "Failed to alloc " << size << " bytes";
+        return NULL;
+    }
+
+    LOG(ERROR) << __func__ << " start";
+    while (uplink_running) {
+        ret = pcm_read(pcm_mic_in, buffer, size);
+        if (ret) {
+            LOG(ERROR) << __func__ << "pcm read failed: " << pcm_get_error(pcm_mic_in);
+            usleep(5000);
+            continue;
+        }
+        ret = pcm_write(pcm_sco_out, buffer, size);
+        if (ret) {
+            LOG(ERROR) << __func__ << "pcm write failed: " << pcm_get_error(pcm_sco_out);
+            usleep(5000);
+            continue;
+        }
+    }
+
+    free(buffer);
+    LOG(ERROR) << __func__ << " stop";
+    return NULL;
+}
+
+void* Bluetooth::downlink_task(void* arg) {
+    Bluetooth* instance = static_cast<Bluetooth*>(arg);
+    instance->downlink_task_impl();
+    return nullptr;
+}
+
+void *Bluetooth::downlink_task_impl()
+{
+    int ret = 0;
+    size_t size = pcm_config_hfp.period_size;
+    void *buffer = malloc(size);
+
+    if (!buffer) {
+        LOG(ERROR) << __func__ << "Failed to alloc " << size << " bytes";
+        return NULL;
+    }
+
+    LOG(ERROR) << __func__ << " start";
+    while (downlink_running) {
+        ret = pcm_read(pcm_sco_in, buffer, size);
+        if (ret) {
+            LOG(ERROR) << __func__ << "pcm read failed: " << pcm_get_error(pcm_sco_in);
+            usleep(5000);
+            continue;
+        }
+        ret = pcm_write(pcm_speaker_out, buffer, size);
+        if (ret) {
+            LOG(ERROR) << __func__ << "pcm write failed: " << pcm_get_error(pcm_speaker_out);
+            usleep(5000);
+            continue;
+        }
+    }
+
+    free(buffer);
+    LOG(ERROR) << __func__ << " stop";
+    return NULL;
+}
+
+void Bluetooth::startHfp() {
+    LOG(DEBUG) << __func__;
+    int ret = 0;
+    ret = openPcmForDevice(AUDIO_DEVICE_OUT_SPEAKER, PCM_OUT,
+                &pcm_config_hfp, &pcm_speaker_out);
+    if (ret)
+        goto error;
+
+    ret = openPcmForDevice(AUDIO_DEVICE_IN_BUILTIN_MIC, PCM_IN,
+                &pcm_config_hfp, &pcm_mic_in);
+    if (ret)
+        goto error;
+
+    ret = openPcmForDevice(AUDIO_DEVICE_OUT_BLUETOOTH_SCO, PCM_OUT,
+                &pcm_config_hfp, &pcm_sco_out);
+    if (ret)
+        goto error;
+
+    ret = openPcmForDevice(AUDIO_DEVICE_IN_BLUETOOTH_SCO_HEADSET, PCM_IN,
+                &pcm_config_hfp, &pcm_sco_in);
+    if (ret)
+        goto error;
+
+    pthread_attr_t attr;
+    struct sched_param schParam;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    schParam.sched_priority = 3;
+    pthread_attr_setschedparam(&attr, &schParam);
+
+    uplink_running = true;
+    ret = pthread_create(&tid_uplink, &attr, &uplink_task, this);
+    if (ret) {
+        goto error;
+    }
+
+    downlink_running = true;
+    ret = pthread_create(&tid_downlink, &attr, &downlink_task, this);
+    if (ret) {
+        goto error;
+    }
+
+    return;
+error:
+    stopHfp();
+    return;
+}
+
 ndk::ScopedAStatus Bluetooth::setHfpConfig(const HfpConfig& in_config, HfpConfig* _aidl_return) {
     if (in_config.sampleRate.has_value() && in_config.sampleRate.value().value <= 0) {
         LOG(ERROR) << __func__ << ": invalid sample rate: " << in_config.sampleRate.value().value;
@@ -67,9 +269,15 @@ ndk::ScopedAStatus Bluetooth::setHfpConfig(const HfpConfig& in_config, HfpConfig
 
     if (in_config.isEnabled.has_value()) {
         mHfpConfig.isEnabled = in_config.isEnabled;
+        if (mHfpConfig.isEnabled == Boolean{true})
+            startHfp();
+        else
+            stopHfp();
+
     }
     if (in_config.sampleRate.has_value()) {
         mHfpConfig.sampleRate = in_config.sampleRate;
+        pcm_config_hfp.rate = mHfpConfig.sampleRate.value().value;
     }
     if (in_config.volume.has_value()) {
         mHfpConfig.volume = in_config.volume;
