a5f852698bd4206260147b7fa6ac240ff0eab084
Jindong Yue <jindong.yue@nxp.com>
2025-04-15 14:50:31 +0800

    MA-23586 audio: Separate multichannel policy from the primary
    Some speaker devices support multichannel like cs42888, but
    if the card is not present, the system still tries to open
    multichannel stream to the speaker and ends up failing.
    Now separate the multichannel policy file from the primay
    policy file.
    
    To enable multichannel policy file, add bootargs
    androidboot.audio.multichannel=1
    
    Change-Id: If095d0d333de7578693b0790086a5df0a95e8928
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/28097
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/main.cpp
+++ audio/default/main.cpp
@@ -25,6 +25,7 @@
 #include <android/binder_ibinder_platform.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+#include <cutils/properties.h>
 
 #include "core-impl/AudioPolicyConfigXmlConverter.h"
 #include "core-impl/ChildInterface.h"
@@ -76,8 +77,17 @@ int main() {
     // Guaranteed log for b/210919187 and logd_integration_test
     LOG(INFO) << "Init for Audio AIDL HAL";
 
-    AudioPolicyConfigXmlConverter audioPolicyConverter{
-            ::android::audio_get_audio_policy_config_file()};
+    static constexpr const char *apmXmlConfigFileName = "audio_policy_configuration.xml";
+    static constexpr const char *apmMultichannelXmlConfigFileName = "audio_policy_configuration_multichannel.xml";
+    std::string file;
+    // Set androidboot.audio.multichannel=1 in bootargs to enable it
+    if (property_get_int32("ro.boot.audio.multichannel", 0)) {
+        file = ::android::audio_find_readable_configuration_file(apmMultichannelXmlConfigFileName);
+    } else {
+        file = ::android::audio_find_readable_configuration_file(apmXmlConfigFileName);
+    }
+    LOG(INFO) << "Load audio policy configuration file from " << file;
+    AudioPolicyConfigXmlConverter audioPolicyConverter{file};
 
     // Make the default config service
     auto config = ndk::SharedRefBase::make<Config>(audioPolicyConverter);
