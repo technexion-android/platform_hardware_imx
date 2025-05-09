8983d08769880bdfd0551fa5d6279d8d54763a62
Jindong Yue <jindong.yue@nxp.com>
2025-05-09 12:01:16 +0800

    MA-23703-2 audio: add retry when detect hdmi connection
    On evk_8mp, sometimes it takes about 1 second to be able
    to detect the connected state.
    Retry several times to make sure the detection is valid.
    
    Change-Id: I32846ff3a5f79a6abf182f4382e8114d6bbe37eb
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/28589
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/ModulePrimary.cpp
+++ audio/default/ModulePrimary.cpp
@@ -101,13 +101,18 @@ ndk::ScopedAStatus ModulePrimary::populateConnectedDevicePort(
 
         mixer = mixer_open(c->card);
         if (mixer) {
-            ctl = mixer_get_ctl_by_name(mixer, "HDMI Jack");
-            if (ctl) {
-                /* If HDMI is connected, return ok */
-                if (mixer_ctl_get_value(ctl, 0)) {
-                    mixer_close(mixer);
-                    return ndk::ScopedAStatus::ok();
+            int retry = 0;
+            while (retry++ <= 10) {
+                ctl = mixer_get_ctl_by_name(mixer, "HDMI Jack");
+                if (ctl) {
+                    /* If HDMI is connected, return ok */
+                    if (mixer_ctl_get_value(ctl, 0) == 1) {
+                        mixer_close(mixer);
+                        return ndk::ScopedAStatus::ok();
+                    }
                 }
+                usleep(200000);
+                LOG(INFO) << __func__ << ": detect HDMI connection, retry " << retry;
             }
             mixer_close(mixer);
         }
